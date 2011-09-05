ae72d5d49136fb87008e34321aa958d890810ef7
unknown <lior@.pbc.local>
2011-09-05 14:46:59 +0300

    Add hebrew translate

---
+++ localization/jquery-ui-timepicker-he.js
@@ -0,0 +1,17 @@
+/* Hebrew translation for the jQuery Timepicker Addon */
+/* Written by Maxim Dolgin */
+(function($) {
+	$.timepicker.regional['he'] = {
+		timeOnlyTitle: 'בחר זמן',
+		timeText: 'זמן',
+		hourText: 'שעות',
+		minuteText: 'דקות',
+		secondText: 'שניות',
+		timezoneText: 'אזור זמן',
+		currentText: 'עכשיו',
+		closeText: 'סגור',
+		timeFormat: 'hh:mm tt',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['he']);
+})(jQuery);
